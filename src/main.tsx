92142231ce718e7bcf747506e1961496804ecccb
Jakub Macek <jakub.macek@smarttech.at>
2025-02-01 10:03:18 +0100

    Trying to get correct routing config with gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import {BrowserRouter} from "react-router";
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<PrimeReactProvider>
-			<BrowserRouter basename="/andor-man-search">
+			<BrowserRouter basename="/">
 				<App />
 			</BrowserRouter>
 		</PrimeReactProvider>

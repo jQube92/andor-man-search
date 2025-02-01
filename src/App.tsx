971f450743f06edba7800f502752bba34eff92fe
Jakub Macek <jakub.macek@smarttech.at>
2025-02-01 10:36:43 +0100

    Trying to get correct routing config with gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import "primeicons/primeicons.css";
-import {Navigate, Route, Routes} from "react-router";
-import ManEntryList from "./components/list/ManEntryList";
-import ManEntryDetails from "./components/details";
+import {Link} from "react-router";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/andor-man-search/#/manual-entries" element={<Navigate to={"/andor-man-search/#/manual-entries"} />} />
-			<Route path="/andor-man-search/#/manual-entries" element={<ManEntryList />} />
-			<Route path="/manual-entries/:entryId" element={<ManEntryDetails />} />
-			<Route path="*" element={<div>not-found </div>} />
-		</Routes>
+		<nav>
+			<Link to="/">Home</Link>
+			<Link to="/page1">Page 1</Link>
+			<Link to="/page2">Page 2</Link>
+		</nav>
 	);
+	// return (
+	// 	<Routes>
+	// 		<Route path="/andor-man-search/#/manual-entries" element={<Navigate to={"/andor-man-search/#/manual-entries"} />} />
+	// 		<Route path="/andor-man-search/#/manual-entries" element={<ManEntryList />} />
+	// 		<Route path="/manual-entries/:entryId" element={<ManEntryDetails />} />
+	// 		<Route path="*" element={<div>not-found </div>} />
+	// 	</Routes>
+	// );
 }
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import "./index.css";
 import App from "./App.tsx";
 import {PrimeReactProvider} from "primereact/api";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-import {HashRouter, Link} from "react-router";
+import {HashRouter, Route, Routes} from "react-router";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<PrimeReactProvider>
-			<HashRouter basename="/">
-				<Link to="/">Home</Link>
-				<App />
+			<HashRouter>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route path="/page1" element={<div>page2</div>} />
+					<Route path="/page2" element={<div>page2</div>} />
+				</Routes>
 			</HashRouter>
 		</PrimeReactProvider>
 	</StrictMode>

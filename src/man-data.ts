4b222646f9deb41bcaba7390afa26f2d8726c90a
Jakub Macek <jakub.macek@smarttech.at>
2025-02-02 16:18:43 +0100

    Added metadata

--- src/man-data.ts
+++ src/man-data.ts
@@ -25,14 +25,68 @@ const data: ManualData[] = [
 	{
 		id: "farmers",
 		title: "Die Bauern",
-		images: ["farmers-0.png"],
+		images: ["farmers-0.png", "farmers-chips-0.png"],
 		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
 	},
 	{
 		id: "creature-movement",
 		title: "Die Bewegung der Kreaturen",
-		images: ["creature-movement-0.png", "creature-movement-1.png"],
+		images: ["creature-movement-0.png", "creature-movement-1.png", "creatures-0.png"],
 		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 2/3`],
+	},
+	{
+		id: "healing-herbs",
+		title: "Die Heilkräuter",
+		images: ["healing-herbs-0.png", "healing-herbs-chips-0.png"],
+		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
+	},
+	{
+		id: "fog-platelets",
+		title: "Die Nebelplättchen",
+		images: ["fog-platelets-0.png", "fog-platelets-chips-0.png"],
+		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
+	},
+	{
+		id: "witch",
+		title: "Die Hexe",
+		images: ["witch-0.png", "witch-chips-0.png"],
+		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
+	},
+	{
+		id: "thorald",
+		title: "Prinz Thorald",
+		images: ["thorald-0.png", "thorald-chips-0.png"],
+		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
+	},
+	{
+		id: "shield-dwarfs",
+		title: "Die Schildzwerge",
+		images: ["shield-dwarfs-0.png", "shield-dwarfs-chips-0.png"],
+		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
+	},
+	{
+		id: "golden-shields",
+		title: "Die goldenen Schilde",
+		images: ["golden-shields-0.png", "golden-shields-chips-0.png"],
+		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
+	},
+	{
+		id: "helmet",
+		title: "Der Helm",
+		images: ["helmet-0.png", "gear-chips-0.png"],
+		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
+	},
+	{
+		id: "checklist",
+		title: "Die Checkliste",
+		images: ["checklist-0.png"],
+		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
+	},
+	{
+		id: "rank-numbers",
+		title: "Die Rangzahlen",
+		images: ["rank-number-0.png"],
+		source: [`${BASE_GAME_ACCOMPANYING_BOOKLET}, S. 3`],
 	},
 ];
 
